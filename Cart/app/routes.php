c2f3d91a02e3f3091dff3a386e5f099473d48eaa
Peter Sacco <peter.sacco@competec.ch>
2016-12-19 10:04:19 +0100

    cart

--- Cart/app/routes.php
+++ Cart/app/routes.php
@@ -1,3 +1,4 @@
 <?php
 
 $app->get('/', ['Cart\Controllers\HomeController', 'execute'])->setName('home');
+$app->get('/products/{slug}', ['Cart\Controllers\ProductController', 'get'])->setName('product.get');
